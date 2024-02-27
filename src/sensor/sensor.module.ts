98c550cc095ef63d8663dc5e8b437e6f3a634579
davidchicano <80907533+davidchicano@users.noreply.github.com>
2024-02-28 00:59:29 +0100

    sensor fix

--- src/sensor/sensor.module.ts
+++ src/sensor/sensor.module.ts
@@ -3,9 +3,13 @@ import { SensorService } from './sensor.service';
 import { SensorController } from './sensor.controller';
 import { Sensor } from './entities/sensor.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Sensor])],
+  imports: [
+    TypeOrmModule.forFeature([Sensor]),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [SensorController],
   providers: [SensorService],
 })
